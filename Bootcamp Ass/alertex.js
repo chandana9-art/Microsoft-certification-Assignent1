8e6037cdf0cd18d28892b63a94a093dc8371ed92
chandana9-art <58177543+chandana9-art@users.noreply.github.com>
2021-06-08 18:45:06 +0530

    Add files via upload

---
+++ Bootcamp Ass/alertex.js
@@ -0,0 +1,1 @@
+alert('Chandana Prasanna Sai Sree');
---
+++ Bootcamp Ass/script.js
@@ -0,0 +1,17 @@
+<script>
+  //function definition
+  function changeColor() {
+     document.getElementById("abcd").style.color="green";
+  }
+  //function definition
+  function pinkColor() {
+     document.getElementById("abcd").style.color="pink";
+  }
+  function colorchange() {
+   document.getElementById("xyz").style.color="blue";
+  }
+
+ </script> 
+ body {
+   
+ }
